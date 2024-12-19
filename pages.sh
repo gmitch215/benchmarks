bdef6d9f393c72e03d47501186f3e39fe47fa587
Gregory Mitchell <me@gmitch215.xyz>
2024-12-18 21:19:16 -0600

    Revert to `pages.sh`

---
+++ pages.sh
@@ -0,0 +1,24 @@
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git fetch origin gh-pages
+
+mkdir -p site
+cp -Rfv build/site/* ./site/
+git switch -f gh-pages
+
+for dir in ./*
+do
+  if [ "$dir" == "./site" ]; then
+    continue
+  fi
+  rm -rfv "$dir"
+done
+
+cp -Rfv ./site/* ./
+rm -rfv ./site
+
+echo "benchmarks.gmitch215.xyz" > CNAME
+
+git add .
+git commit -m "Update Benchmarks Site"
+git push -f origin gh-pages
