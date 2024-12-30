98ed1f97cd108e91cd714a6a33a58ec410950ac6
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-12-30 03:48:15 -0600

    Add `sha1-hashing` Benchmark

---
+++ benchmarks/sha1-hashing/main.kt
@@ -0,0 +1,53 @@
+@file:OptIn(
+    dev.whyoleg.cryptography.DelicateCryptographyApi::class,
+    dev.whyoleg.cryptography.CryptographyProviderApi::class,
+    kotlin.io.encoding.ExperimentalEncodingApi::class
+)
+
+import dev.whyoleg.cryptography.CryptographyProvider
+import dev.whyoleg.cryptography.algorithms.SHA1
+import kotlin.time.measureTime
+import kotlinx.coroutines.runBlocking
+
+val phrases = listOf(
+    "Seize the day",
+    "Time flies quickly",
+    "Reach for the stars",
+    "Love conquers all",
+    "Break the ice",
+    "A blessing in disguise",
+    "Actions speak louder than words",
+    "Every cloud has a silver lining",
+    "Burn the midnight oil",
+    "Jack of all trades",
+    "Piece of cake",
+    "Bite the bullet",
+    "Crystal clear",
+    "Better late than never",
+    "The early bird catches the worm",
+    "Cat got your tongue",
+    "A penny for your thoughts",
+    "Laughter is the best medicine",
+    "All ears",
+    "Barking up the wrong tree",
+    "Beauty is in the eye of the beholder",
+    "The calm before the storm",
+    "Let the cat out",
+    "On thin ice",
+    "Cut to the chase"
+)
+
+fun main() {
+    val hasher = CryptographyProvider.Default
+        .get(SHA1)
+        .hasher()
+
+    val time = measureTime {
+        runBlocking {
+            for (phrase in phrases)
+                hasher.hash(phrase.encodeToByteArray())
+        }
+    }
+
+    println(time.inWholeNanoseconds)
+}
---
+++ benchmarks/sha1-hashing/main.php
@@ -0,0 +1,39 @@
+<?php
+
+$phrases = [
+    "Seize the day",
+    "Time flies quickly",
+    "Reach for the stars",
+    "Love conquers all",
+    "Break the ice",
+    "A blessing in disguise",
+    "Actions speak louder than words",
+    "Every cloud has a silver lining",
+    "Burn the midnight oil",
+    "Jack of all trades",
+    "Piece of cake",
+    "Bite the bullet",
+    "Crystal clear",
+    "Better late than never",
+    "The early bird catches the worm",
+    "Cat got your tongue",
+    "A penny for your thoughts",
+    "Laughter is the best medicine",
+    "All ears",
+    "Barking up the wrong tree",
+    "Beauty is in the eye of the beholder",
+    "The calm before the storm",
+    "Let the cat out",
+    "On thin ice",
+    "Cut to the chase"
+];
+
+$before = hrtime(true);
+
+for ($i = 0; $i < 25; $i++) {
+    sha1($phrases[$i]);
+}
+
+$after = hrtime(true);
+
+echo $after - $before;
