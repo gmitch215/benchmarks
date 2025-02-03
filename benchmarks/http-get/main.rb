d1a77ca24a8dd89a16acd74392a344b76cb24179
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-02-02 20:55:47 -0600

    fix: Ruby HTTP Exceptions

--- benchmarks/http-get/main.rb
+++ benchmarks/http-get/main.rb
@@ -2,8 +2,11 @@ require 'net/http'
 
 before = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
 
-Net::HTTP.get('httpbin.org', '/get')
-
+begin
+  Net::HTTP.get('httpbin.org', '/get')
+rescue Exception
+  false
+end
 after = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
 
 puts after - before
--- benchmarks/http-post/main.rb
+++ benchmarks/http-post/main.rb
@@ -2,14 +2,18 @@ require 'net/http'
 
 before = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
 
-uri = URI.parse('http://example.com/your-endpoint')
+uri = URI.parse('http://httpbin.org/post')
 
 request = Net::HTTP::Post.new(uri)
 request['Content-Type'] = 'text/plain'
 request.body = 'Hello World'
 
-Net::HTTP.start(uri.host, uri.port) do |http|
-  http.request(request)
+begin
+  Net::HTTP.start(uri.host, uri.port) do |http|
+    http.request(request)
+  end
+rescue Exception
+  false
 end
 
 after = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
